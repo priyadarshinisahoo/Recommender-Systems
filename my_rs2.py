c8e916541fd0ca727c55ecb984a52a133bec5d2b
Priyadarshini Sahoo <priyadarshinis77@gmail.com>
2017-06-27 18:04:22 +0530

    "some functions have been modified"

--- my_rs2.py
+++ my_rs2.py
@@ -7,22 +7,22 @@ Created on Mon Jun 19 10:56:33 2017
 
 import pandas as pd
 import numpy as np
-from collections import defaultdict
+import math
 from sklearn.model_selection import train_test_split
 
 r_cols = ['user_id','movie_id','rating','timestamp']
 ratings = pd.read_csv('D:\Python\ml-100k\u.data', sep ='\t', names= r_cols)
 
-print ratings.head()
+#print ratings.head()
 
 ratings.drop(['timestamp'], 1, inplace= True)
-print ratings.head()
-ratings
+#print ratings.head()
+
 
 num_users = ratings.user_id.unique().shape[0]
-print num_users
+print "number of users: ",num_users
 num_items = ratings.movie_id.unique().shape[0]
-print num_items
+print "number of items: ",num_items
 
 """
 User-based Recommendation
@@ -30,8 +30,24 @@ User-based Recommendation
 """
 
 train_data, test_data = train_test_split(ratings, test_size=0.25) #train-test set split
-print train_data.head()
-print test_data.head()
+#print train_data.head()
+#print test_data.head()
+
+"""ratings_data= {}
+for row in test_data.itertuples():
+    user_data_test.setdefault(row[1],{})
+    user_data_test[row[1]].setdefault(row[2],row[3])
+"""
+
+user_data= {}
+for row in train_data.itertuples():
+    user_data.setdefault(row[1],{})
+    user_data[row[1]].setdefault(row[2],row[3])
+    
+user_data_test= {}
+for row in test_data.itertuples():
+    user_data_test.setdefault(row[1],{})
+    user_data_test[row[1]].setdefault(row[2],row[3])
 
 num_ratings_train = train_data.rating.shape[0]
 num_ratings_test = test_data.rating.shape[0]
@@ -39,10 +55,7 @@ num_ratings_test = test_data.rating.shape[0]
 #add = defaultdict(list)
 #print type(train_data)
 
-user_data= {}
-for row in train_data.itertuples():
-    user_data.setdefault(row[1],{})
-    user_data[row[1]].setdefault(row[2],row[3])
+
 
 '''
 u1 = train_data.set_index('user_id', drop=True).to_dict(orient='index')
@@ -68,15 +81,24 @@ for key in user_data:
         user_data[key].update({train_data.movie_id: train_data.rating})
         
 '''
+def r_average(user):
+    sums=0
+    for item in user_data[user].keys():
+        sums = sums + user_data[user][item]
+    no_of_ratings= len(user_data[user].keys())
+    average = float(sums/no_of_ratings)
+    return average
+
 
 #k-neighbors
+#in the form (usr2:msd)
 def knn_func(user1, k):
     w = {}
     for u2 in user_data.keys():
         if (u2==user1):
             continue
         else:
-            w.update({u2: float(msd_sim(user1,u2))})
+            w.update({u2: pc_sim(user1,u2)})
     
     sorted_w = sorted(w.items(), key=lambda x:x[1], reverse=True)
     if (len(sorted_w)<k):
@@ -85,33 +107,48 @@ def knn_func(user1, k):
     return sorted_w
 
 #computing similarity
-def msd_sim(user1, user2):
-    I= 0
-    sqd_diff = 0.0
+#msd = |I[u][v]|/summation((r[u][i]- r[v][i])**2)
+def pc_sim(user1, user2):
+    top=0
+    bottom1=0
+    bottom2=0
+    r_u_avg= r_average(user1)
+    r_v_avg= r_average(user2)
     for i in user_data[user1].keys():
-        if ( i in user_data[user2].keys()):
-            I= I+1
-            sqd_diff = sqd_diff + float((user_data[user1][i] - user_data[user2][i])**2)
-    if (sqd_diff == 0):
-        return 1
+        if (i in user_data[user2].keys()):
+            top= top + (user_data[user1][i] - r_u_avg)*(user_data[user2][i] - r_v_avg)
+            bottom1= bottom1 + (user_data[user1][i] - r_u_avg)**2
+            bottom2 = bottom2 + (user_data[user2][i] - r_v_avg)**2
+    div = float(math.sqrt(bottom1*bottom2))
+    if (div != 0):
+        pc= float(top/div)
     else:
-        msd = float(I/sqd_diff)
-        return msd
+        pc = 1
+    return pc
+
 w ={}
 
 #w = knn_func(21, 7)
 #print w
-
-import math    
+#item is predicted if predicted rating of the item is greater than 3.8
+   
 pred_items = []
-def recomm(user):
-    for i in user_data[user].keys():
-        pred = pred_ratings(user, i)
-        if ( pred >= 3.8):
-          pred_items.append(i)
-    return pred_items
-     
 
+def recomm(user):
+    dict_pred ={}
+    for i in ratings.movie_id.unique():
+        if (i in user_data[user].keys()):
+            continue
+        else:
+            pred = pred_ratings(user, i)
+            if ( pred >= 3.8):
+                dict_pred.update({i:pred})
+    sorted_dict = sorted(dict_pred.items(), key=lambda x:x[1], reverse=True)
+    for k in sorted_dict:
+        pred_items.append(k[0]) 
+    return pred_items
+
+#pred_ratings = summation(w[u][v] * r[v][i])/summation(|w[u][v]|)
 def pred_ratings(user1, item):
     w1= {}
     w1.update(knn_func(user1,12))
@@ -133,28 +170,43 @@ def pred_ratings(user1, item):
         ratings = mul/div
     return ratings
 
-def accuracy(user):
+#mae = summation(|predicted_ratings - actual_ratings|)
+#accuracy = 1-mae
+def mean_absolute_error(user):
     sums=0
     num_ratings = num_ratings_train
-    for i in user_data[user].keys():
-        sums = sums + math.fabs(pred_ratings(user,i) - user_data[user][i])
+    for i in user_data_test[user].keys():
+        sums = sums + math.fabs(pred_ratings(user,i) - user_data_test[user][i])
     mae = float(sums/ num_ratings)
-    accuracy = (1-mae)*100
-    return float(accuracy)
+    return mae
 
 #print pred_ratings(21,8)
 
 #print accuracy(21,17)
 
-def precision(user):
+#precision = total correctly predicted items/ total recommended items
+def precision(user, recommend):
     corr_items = 0
-    total_recomm = len(recomm(user))
-    for i in recomm(user):
-        if (user_data[user][i]>3.8):
+    total_recomm = len(recommend)
+    for i in user_data_test[user].keys():
+        if (i in recommend and user_data_test[user][i]>3.8):
             corr_items= corr_items +1
-    precision_u= corr_items/total_recomm
+    precision_u= float(corr_items/total_recomm)
     return precision_u
+
 user = input("Enter the user id")
-print recomm(int(user))
-print accuracy(user) 
-print precision(user)
+recommend = recomm(int(user))
+print "recommendations: ", recommend
+print "mean absolute error: ", mean_absolute_error(int(user))
+print "precision: ",precision(int(user), recommend)
+
+
+
+"""
+
+for row in test_data.itertuples():
+    user = row[1]
+    print "for the user_id, " + str(user) + " recommended item_ids are:"
+    print recomm(user)
+    
+"""
