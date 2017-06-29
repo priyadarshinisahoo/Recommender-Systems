1300a1c46e06c510a5592a2a4416f54266b85e85
Priyadarshini Sahoo <priyadarshinis77@gmail.com>
2017-06-30 00:39:42 +0530

    "recommendations for top 100 users"

--- my_rs2.py
+++ my_rs2.py
@@ -10,6 +10,11 @@ import numpy as np
 import math
 from sklearn.model_selection import train_test_split
 
+mae=[]
+prec=[]
+rec=[]
+rmse=[]
+
 r_cols = ['user_id','movie_id','rating','timestamp']
 ratings = pd.read_csv('D:\Python\ml-100k\u.data', sep ='\t', names= r_cols)
 
@@ -44,9 +49,9 @@ user_data_test= {}
 for row in test_data.itertuples():
     user_data_test.setdefault(row[1],{})
     user_data_test[row[1]].setdefault(row[2],row[3])
-
-num_ratings_train = train_data.rating.shape[0]
-num_ratings_test = test_data.rating.shape[0]
+    
+#num_ratings_train = train_data.rating.shape[0]
+#num_ratings_test = test_data.rating.shape[0]
 
 '''
 with open(train_data) as f1:
@@ -71,12 +76,12 @@ def r_average(user):
 
 #k-neighbors
 #in the form {(usr2:msd)}
-def knn_func(user1, k):
+def knn_func(user1,item,k):
     w = {}
     for u2 in user_data.keys():
         if (u2==user1):
             continue
-        else:
+        if(item in user_data[u2].keys()):
             w.update({u2: pc_sim(user1,u2)})
     
     sorted_w = sorted(w.items(), key=lambda x:x[1], reverse=True)
@@ -108,17 +113,13 @@ def pc_sim(user1, user2):
 w ={}
 
 #item is predicted if predicted rating of the item is greater than 3.8
-pred_items = []
 
 def recomm(user):
     dict_pred ={}
-    for i in ratings.movie_id.unique():
-        if (i in user_data[user].keys()):
-            continue
-        else:
-            pred = pred_ratings(user, i)
-            if ( pred >= 3.8):
-                dict_pred.update({i:pred})
+    for i in user_data_test[user].keys():
+        pred = pred_ratings(user, i)
+        if ( pred >= 3.8):
+            dict_pred.update({i:pred})
     sorted_dict = sorted(dict_pred.items(), key=lambda x:x[1], reverse=True)
     for k in sorted_dict:
         pred_items.append(k[0]) 
@@ -127,7 +128,7 @@ def recomm(user):
 #pred_ratings = summation(w[u][v] * r[v][i])/summation(|w[u][v]|)
 def pred_ratings(user1, item):
     w1= {}
-    w1.update(knn_func(user1,100))
+    w1.update(knn_func(user1,item,1000))
     mul=0
     div=0
     for v in w1.keys():
@@ -140,33 +141,57 @@ def pred_ratings(user1, item):
         ratings = mul/div
     return ratings
 
-#mae = summation(|predicted_ratings - actual_ratings|)
+#mae = summation(|predicted_ratings - actual_ratings|)/ no.of ratings by user on item i
 def mean_absolute_error(user):
     sums=0
-    num_ratings = num_ratings_test
+    sq=0
+    num_ratings = len(user_data_test[user].keys())
     for i in user_data_test[user].keys():
-        sums = sums + math.fabs(pred_ratings(user,i) - user_data_test[user][i])
-    mae = float(sums/ num_ratings)
-    return mae
+        p = pred_ratings(user,i)
+        u= user_data_test[user][i]
+        sums = sums + math.fabs(p-u)
+        sq= sq + (p- u)**2
+    err = float(sums/ num_ratings)
+    mae.append(err)
+    rm= math.sqrt(float(sq/num_ratings))
+    rmse.append(rm)
 
 #precision = total correctly predicted items/ total recommended items
+#recall = total correctly predicted items/total liked items
 def precision(user, recommend):
     tp=0.0
     fp=0.0
     fn=0.0
-    for i in ratings.movie_id.unique():
-        if (i in recommend and user_data_test[user]>3.8):
+    for i in user_data_test[user].keys():
+        if (i in recommend and user_data_test[user][i]>3.8):
             tp=tp+1
-        elif(i in recommend and user_data_test[user]<3.8):
+        elif(i in recommend and user_data_test[user][i]<3.8):
             fp=fp+1
-        elif(i not in recommend and user_data_test[user]>3.8):
+        elif(i not in recommend and user_data_test[user][i]>3.8):
             fn=fn+1
-    precision = tp/(tp+fp)
-    recall = tp/(tp+fn)
-    return precision,recall
+    if(tp+fp==0):
+        precision=0
+    else:
+        precision = tp/(tp+fp)
+    if (tp+fn==0):
+        recall=0
+    else:
+        recall = tp/(tp+fn)
+    prec.append(precision)
+    rec.append(recall)
+n=0
 
-user = input("Enter the user id")
-recommend = recomm(int(user))
-print "recommendations: ", recommend
-print "mean absolute error: ", mean_absolute_error(int(user))
-print "precision and recall:",precision(int(user), recommend)
+for x in user_data_test.keys():
+    pred_items=[]
+    recommend = recomm(x)
+    print "Recommendations for user id ", x, ": ", recommend
+    precision(x, recommend)
+    mean_absolute_error(x)
+    n=n+1
+    if(n>=100):
+        break;
+        
+print "precision: ", sum(prec)/n
+print "recall: ", sum(rec)/n
+print "mean absolute error: ", sum(mae)/n
+print "rmse: ", sum(rmse)/n
